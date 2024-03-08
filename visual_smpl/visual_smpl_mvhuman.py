98d9dbc5e45b152c08e198c35f69cb3a1cbde756
X-zhangyang <33534662+X-zhangyang@users.noreply.github.com>
2024-03-08 17:06:16 +0800

    Update visual_smpl_mvhuman.py

--- visual_smpl/visual_smpl_mvhuman.py
+++ visual_smpl/visual_smpl_mvhuman.py
@@ -6,11 +6,11 @@ import smplx
 import pickle
 
 from scipy.spatial.transform import Rotation
-import pickle
+# import pickle
 import cv2
 from utils_mvhuman import rotation_matrix_to_angle_axis, transform_can_smpl
 # import SMPL
-import json
+# import json
 
 model_smplx = smplx.create(os.path.expanduser(r"smpl_\SMPL_NEUTRAL.pkl"),model_type="smplx",)
 
