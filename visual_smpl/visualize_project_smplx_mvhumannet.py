17be03fbdf9931ed9bc19cd51f6d846eeb489c86
X-zhangyang <33534662+X-zhangyang@users.noreply.github.com>
2024-06-05 21:51:45 +0800

    Add files via upload

--- visual_smpl/visualize_project_smplx_mvhumannet.py
+++ visual_smpl/visualize_project_smplx_mvhumannet.py
@@ -1,32 +1,48 @@
-from mytools.camera_utils import read_camera_ours
+from mytools.camera_utils import read_camera_mvhumannet
 from mytools.writer import FileWriter
 import trimesh
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+import pickle
 
-projection_results = "projection_results.jpg"
+projection_results = "projection_result.jpg"
 
 
 
 img_path =  r'data/0380_img.jpg'
-# mask_path =   
-
 obj_path = r'data/000075_smplx.obj'
 intri_name = r'data/camera_intrinsics.json'
 extri_name =  r'data/camera_extrinsics.json'
-cameras_gt = read_camera_ours(intri_name, extri_name)
+camera_render_list = ["CC32871A059",]
+camera_scale_fn = r'data/camera_scale.pkl'
 
+# camera_render_list = ["22236230",]
+# img_path =  r'E:\Program_4Dhuman_release\lg_samples_203331\203331\images_lr\22236230\0005_img.jpg'
+# obj_path = r'E:\Program_4Dhuman_release\lg_samples_203331\203331\smplx\smplx_mesh\000000.obj'
+# intri_name = r'E:\Program_4Dhuman_release\lg_samples_203331\203331\camera_intrinsics.json'
+# extri_name =  r'E:\Program_4Dhuman_release\lg_samples_203331\203331\camera_extrinsics.json'
+# camera_scale_fn = r'E:\Program_4Dhuman_release\lg_samples_203331\203331\camera_scale.pkl'
+
+
+
+
+camera_scale = pickle.load(open(camera_scale_fn, "rb"))
+if camera_scale ==120 / 65:
+    image_size = [4096, 3000]
+else:
+    image_size = [2448, 2048]
+
+
+
+
+cameras_gt = read_camera_mvhumannet(intri_name, extri_name, camera_scale)
 obj_data = trimesh.load(obj_path)
-
 vertices = np.array(obj_data.vertices)
 faces = np.array(obj_data.faces)
-
-
-
-camera_render_list = ["CC32871A059",]
 cam_i = camera_render_list
+
 
 render_data = {}
 assert vertices.shape[1] == 3 and len(vertices.shape) == 2, 'shape {} != (N, 3)'.format(vertices.shape)
@@ -49,7 +65,7 @@ write_smpl  = FileWriter(r"/", config=config)
 data_images = cv2.imread(img_path)
 render_data_input = {"0":render_data}
 shape_ori = data_images.shape
-data_images_input = cv2.resize(data_images, [4096,3000])
+data_images_input = cv2.resize(data_images, [image_size[0],image_size[1]])
 smpl_img = write_smpl.vis_smpl(render_data_input, [data_images_input], cameras, outname_cache, add_back=False)
 
 
@@ -57,8 +73,7 @@ smpl_img = write_smpl.vis_smpl(render_data_input, [data_images_input], cameras, 
 
 
 
-
-smpl_img_re = cv2.resize(smpl_img, (4096, 3000), interpolation=cv2.INTER_AREA)
+smpl_img_re = cv2.resize(smpl_img, (image_size[0],image_size[1]), interpolation=cv2.INTER_AREA)
 smpl_mask = np.repeat(smpl_img_re[:,:,3:],3,2)/255
 data_save = data_images_input * (1-smpl_mask) + smpl_img_re[:,:,:3]* (smpl_mask)
 
